92c2cfca047706e8a9f0f3f2732354f3d759fd3a
somteacodes <stomteacodes@gmail.com>
2020-12-26 00:51:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# GeekKing-Tutorial
+
+Login Screen from Adobe Photoshop to Android Studio
+![preview image]https://i9.ytimg.com/vi/WbbGYz6o54U/maxresdefault.jpg
---
+++ app/src/main/java/org/eliconcepts/geekking/MainActivity.java
@@ -0,0 +1,16 @@
+package org.eliconcepts.geekking;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setBackgroundDrawableResource(R.drawable.background);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+}
